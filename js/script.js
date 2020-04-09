3adac1a69f0e14dc0acaee5f75a7259b55efbadf
Devang Chauhan <devngc@gmail.com>
2020-04-09 04:42:58 -0400

    Hydra like Demo
    This is something really promising.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
-for (i=0; i<100; i++){
-  var request = new XMLHttpRequest();
-  request.open ('GET', 'https://github.com/devngc/repo/blob/gh-pages/data.txt', false);
-  request.send();
-  if (request.status == 200){
-    console.log(request);
-    document.writeln(request.responseText);
-  }
-}
+
--- update.py
+++
@@ -1,9 +0,0 @@
-"""This script takes add extra element to the HTML file to update the webpage"""
-r = open("test.html", "w+")
-if r.mode == "r":
-    fl = r.readlines()
-    for x in fl:
-        print x
-    for i in range(10):
-        r.write("This is line number {} \r\n".format(i))
-r.close()
