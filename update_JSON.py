2060400b70314eabc1be36f55377fa7e0faaa92c
Devang Chauhan <devngc@gmail.com>
2020-04-12 13:23:28 -0400

    JSON updater initialized

---
+++ update_JSON.py
@@ -0,0 +1,11 @@
+"""This script maps the folders in data folder to a JSON file"""
+import json
+import platform
+import os
+
+print("This is python version {}".format(platform.python_version()))
+print("current working directory is {}".format(os.getcwd()))
+
+
+def writeJSON(path, filename, data):
+    pass
